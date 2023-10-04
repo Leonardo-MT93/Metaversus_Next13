c40a48e7773950a214fa11dc2a23c68cfe3b9afc
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-10-03 23:13:46 -0300

    Fixing HeroSection

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -13,11 +13,11 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+        <motion.h1 variants={textVariant(0.25)} className={styles.heroHeading}>
           Metaverse
         </motion.h1>
         <motion.div
-          variants={textVariant(1.2)}
+          variants={textVariant(0.25)}
           className="flex flex-row justify-center items-center"
         >
           <h1 className={styles.heroHeading}>Ma</h1>
